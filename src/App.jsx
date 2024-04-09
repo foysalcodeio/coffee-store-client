a8645334cd5ea7ed22a5942b8b71605f92e5816e
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-04-10 03:20:57 +0600

    working on grid design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import CoffeeCard from "./components/CoffeeCard";
 
 
 function App() {
@@ -7,7 +8,18 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl  text-purple-600">Coffees : {coffees.length}</h1>
+      <div className="m-20">
+        <h1 className="text-6xl text-center my-20 text-purple-600">Coffees : {coffees.length}</h1>
+        <div className="grid md:grid-cols-2 gap-4">
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+          ></CoffeeCard>)
+        }
+        </div>        
+      </div>
+      
     </>
   )
 }
