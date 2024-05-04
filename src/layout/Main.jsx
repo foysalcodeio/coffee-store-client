4b4a1cb5a7eb57eb50a5addb8cc3fcdc1bd10db7
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-05-04 16:45:06 +0600

    created

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import NavbarH from "../components/NavbarH";
+import Footer from "../components/Footer";
 
 const Main = () => {
     return (
         <div className='mx-auto'>
             <Navbar></Navbar>
             <NavbarH></NavbarH>
-            <Outlet></Outlet>           
+            <Outlet></Outlet>   
+            <Footer></Footer>        
         </div>
     );
 };
